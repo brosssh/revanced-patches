f203bac8584720414bd7905beeb55d9d9ed3be59
brosssh <tiabroch@gmail.com>
2024-11-18 14:44:31 +0100

    fix: Testing semantic release

--- patches/src/main/kotlin/app/revanced/patches/example/ExamplePatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/example/ExamplePatch.kt
@@ -12,6 +12,6 @@ val examplePatch = bytecodePatch(
     extendWith("extensions/extension.rve")
 
     execute {
-        // TODO("Not yet implemented")
+        // TODO("Not yet implemented"
     }
 }
