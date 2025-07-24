6b54d43f3fac257653e990954a530dd1c1845dab
brosssh <tiabroch@gmail.com>
2025-07-24 12:43:13 +0200

    feat(park4night): Pro patch for park4night

---
+++ patches/src/main/kotlin/app/revanced/patches/park4night/subscription/UnlockProPatch.kt
@@ -0,0 +1,16 @@
+package app.revanced.patches.park4night.subscription
+
+import app.revanced.patcher.extensions.InstructionExtensions.addInstruction
+import app.revanced.patcher.patch.bytecodePatch
+
+@Suppress("unused")
+val unlockProPatch = bytecodePatch(
+    name = "Unlock Pro features",
+    description = "Unlock Pro features (park4night +)"
+) {
+    compatibleWith("fr.tramb.park4night"("7.1.11"))
+
+    execute {
+        userDtoConstructorFingerprint.method.addInstruction(0, "const-string p21, \"2050-01-01\"")
+    }
+}
